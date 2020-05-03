37eb9e51eea1814ae9d02ef51f0fa819b5a61274
changhoho <changhoho@github.com>
2020-05-03 21:02:15 +0900

    preprocessing

---
+++ preprocessing.py
@@ -0,0 +1,27 @@
+import json
+
+"""
+    곡: idx
+    장르: idx
+    태그: idx
+"""
+if __name__=="__main__":
+
+    with open('./dataset/song_meta.json') as json_file:
+        json_data = json.load(json_file)
+
+    with open('./dataset/song_meta.json') as json_file:
+        song = json.load(json_file)
+
+
+
+
+
+
+
+
+
+
+
+
+
--- split_data.py
+++ split_data.py
@@ -3,7 +3,7 @@ import json
 if __name__=="__main__":
 
 
-    with open('./data/train.json') as json_file:
+    with open('./dataset/train.json') as json_file:
         json_data = json.load(json_file)
 
     print(json_data[:10])
